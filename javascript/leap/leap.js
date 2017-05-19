f43d59a04dad4f9ddc833accbf1b5beb5eb41bef
Filipe Botti <filipebotti@hotmail.com>
2017-05-19 16:33:36 -0300

    leap exercise

---
+++ javascript/leap/leap.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function(input) {
+    this.value = input;
+};
+
+Year.prototype.isLeap = function() {
+
+    return this.value % 4 == 0 && 
+            ((this.value % 100 != 0 || this.value % 100 == 0 && this.value % 400 == 0));    
+};
+
+module.exports = Year;
