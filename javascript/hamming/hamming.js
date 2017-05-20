937d4ffafa7f44a5fd869a64ec994043d0922d22
Filipe Botti <filipebotti@hotmail.com>
2017-05-20 20:11:15 -0300

    updates hamming

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -5,8 +5,8 @@ Hamming.prototype.compute = function(firstDna, secondDna) {
     if(firstDna.length !== secondDna.length)
         throw new Error('DNA strands must be of equal length.');
 
-    let difference = 0;
-    for(let i = 0; i < firstDna.length; i++) {
+    var difference = 0;
+    for(var i = 0; i < firstDna.length; i++) {
         if(firstDna[i] !== secondDna[i])
             difference++;
     }
